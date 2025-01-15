d71677ea3512ddd0f31a3dc7d8c4dd5bef53814e
Rocío Cejudo <38685526+rcellas@users.noreply.github.com>
2025-01-15 07:09:17 +0100

    34 logica del temporizador (#60)
    * Merge pull request #45 from juanppdev/develop (#46)
    
    * add functionality for pomodoro
    
    * add styles to pomodoro
    
    * fix error styles and add input label for change time
    
    ---------
    
    Co-authored-by: Juan Pablo Patino Lopez <juanppdev@gmail.com>

--- app/src/main/java/com/mundocode/proyecto_1_pomodoro/MainActivity.kt
+++ app/src/main/java/com/mundocode/proyecto_1_pomodoro/MainActivity.kt
@@ -7,41 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.mundocode.proyecto_1_pomodoro.ui.screens.timer.TimerScreen
 import com.mundocode.proyecto_1_pomodoro.ui.theme.Proyecto_1_PomodoroTheme
+import com.mundocode.proyecto_1_pomodoro.ui.viewmodel.TimerViewModel
 
 class MainActivity : ComponentActivity() {
+    private val timerViewModel = TimerViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Proyecto_1_PomodoroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TimerScreen(
+                       viewModel = timerViewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Proyecto_1_PomodoroTheme {
-        Greeting("Android")
     }
 }
