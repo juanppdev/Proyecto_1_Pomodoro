4887cd01fe3bac8a5d8d0b7b46a572dea8f76b86
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-01-15 07:23:18 +0100

    Update .gitignore and add preview to Timer Screen
    - Update .gitignore to include more files and folders to ignore
    - Add a preview to the `TimerScreen` Composable function for better development visualization.

--- app/src/main/java/com/mundocode/proyecto_1_pomodoro/ui/screens/timer/Timer.kt
+++ app/src/main/java/com/mundocode/proyecto_1_pomodoro/ui/screens/timer/Timer.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mundocode.proyecto_1_pomodoro.R
@@ -116,4 +117,10 @@ fun TimerScreen(viewModel:TimerViewModel, modifier: Modifier) {
 
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TimerScreenPreview() {
+    TimerScreen(viewModel = TimerViewModel(), modifier = Modifier)
 }
