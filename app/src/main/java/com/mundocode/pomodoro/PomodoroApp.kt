bfc5e9b7a0cef53f715f34506d2fc2e6fe8ca137
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-03-14 22:15:18 +0100

    feat: Implement Crash Reporting with Timber and Crashlytics
    - Added `CrashReportingTree` to handle crash reporting via Firebase Crashlytics.
    - Configured Timber to use `CrashReportingTree` in release builds and `DebugTree` in debug builds.
    - Implemented custom key settings for priority, tag, and message in crash logs.
    - Recorded exceptions and logs in `CrashReportingTree`.
    - Planted `CrashReportingTree` and `DebugTree` on application startup.

--- app/src/main/java/com/mundocode/pomodoro/PomodoroApp.kt
+++ app/src/main/java/com/mundocode/pomodoro/PomodoroApp.kt
@@ -1,11 +1,20 @@
 package com.mundocode.pomodoro
 
 import android.app.Application
+import com.mundocode.pomodoro.core.CrashReportingTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class PomodoroApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(
+            if (BuildConfig.DEBUG) {
+                Timber.DebugTree()
+            } else {
+                CrashReportingTree()
+            },
+        )
     }
 }
